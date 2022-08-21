026164712892288e885a80487a04ae7ba8862548
Dave <psychostasia1@gmail.com>
2022-08-21 17:37:41 -0500

    basic flash notice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,7 +40,8 @@ class MessagesController < ApplicationController
             turbo_stream.prepend('messages', 
                   partial: "messages/message", 
                   locals: {message: @message}),
-            turbo_stream.update('message_counter', Message.count)           
+            turbo_stream.update('message_counter', Message.count),
+            turbo_stream.update('notice', "Message #{@message.id} created")            
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
@@ -64,10 +65,13 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.update(message_params)
         format.turbo_stream do 
-          render turbo_stream: turbo_stream.update(@message,
+          render turbo_stream: [
+            turbo_stream.update(@message,
                                 partial: "messages/message",
-                                locals: {message: @message})
-        end
+                                locals: {message: @message}),
+            turbo_stream.update('notice', "Message #{@message.id} updated")
+                 ]     
+                  end
         format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
         format.json { render :show, status: :ok, location: @message }
       else
@@ -90,7 +94,8 @@ class MessagesController < ApplicationController
       format.turbo_stream do 
         render turbo_stream: [
           turbo_stream.remove(@message),
-          turbo_stream.update('message_counter', Message.count)
+          turbo_stream.update('message_counter', Message.count),
+          turbo_stream.update('notice', "Message #{@message.id} deleted")
         ]
       end
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
